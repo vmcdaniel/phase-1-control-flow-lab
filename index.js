778d88682870a8fd07b24c4836f2457fc1a6e652
Victoria <vmcdaniel459@gmail.com>
2024-07-13 12:17:12 -0400

    good job

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(feetLength) {
+  if (feetLength <= 400) {
+  return 'This one is on me!'; 
+  }
+  else if(feetLength >= 400 && feetLength <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feetLength > 2500) {
+    return 'No can do.';
+  }
+  else if (feetLength > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? 'Ok, sounds good.' :'No go.';
+  //return (city === 'Pittsburgh')  ? 'No go.' : 'Lets go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){;
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;   
+        case 'thanks for everything':
+          return 'Bye.'
+          break;
 }
+} 
